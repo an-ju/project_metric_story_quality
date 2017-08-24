71e90bd299781a310e6803424ddee837d55ae447
an-ju <an_ju@berkeley.edu>
2017-08-24 13:01:51 -0700

    Version 0.1.0

---
+++ lib/project_metric_story_quality.rb
@@ -0,0 +1,47 @@
+require "project_metric_story_quality/version"
+require 'faraday'
+require 'json'
+
+class ProjectMetricStoryQuality
+  def initialize(credentials, raw_data = nil)
+    @project = credentials[:tracker_project]
+    @conn = Faraday.new(url: 'https://www.pivotaltracker.com/services/v5')
+    @conn.headers['Content-Type'] = 'application/json'
+    @conn.headers['X-TrackerToken'] = credentials[:tracker_token]
+    @raw_data = raw_data
+  end
+
+  def image
+    refresh unless @raw_data
+    { chartType: 'story_quality',
+      titleText: 'Story Quality',
+      data: @raw_data }.to_json
+  end
+
+  def refresh
+    @raw_data = stories
+  end
+
+  def raw_data=(new)
+    @raw_data = new
+    @score = nil
+    @image = nil
+  end
+
+  def score
+    refresh unless @raw_data
+    @score = @raw_data.length
+  end
+
+  def self.credentials
+    %I[tracker_project tracker_token]
+  end
+
+  private
+
+  def stories
+    JSON.parse(
+        @conn.get("projects/#{@project}/stories").body
+    )
+  end
+end
---
+++ lib/project_metric_story_quality/version.rb
@@ -0,0 +1,3 @@
+class ProjectMetricStoryQuality
+  VERSION = "0.1.0"
+end
